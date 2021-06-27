ee19d2d001e6943c14903e685fd8668652b87f4a
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-27 10:34:39 +0530

    enqueue is added

--- Runner.java
+++ Runner.java
@@ -1,6 +1,6 @@
-/***********************************************************************
- * In this code, stack of integers 56,30 and 70 is created and ability
- * to peek and pop is added.
+/************************************************************************
+ * In this code, queue of integers 56,30 and 70 is created and enqueue is
+ * utilized. 
  * 
  * @author Pratik Chaudhari
  * @since 26/06/2021
@@ -9,16 +9,10 @@ package com.bridgelabz;
 
 public class Runner {
 	public static void main(String[] args) {
-		Stack<Integer> stack = new Stack<Integer>();
-		stack.push(70);
-		stack.push(30);
-		stack.push(56);
-		stack.show();
-		System.out.println();
-		while (stack.peek() != null) {
-			System.out.println("First element of the stack is " + stack.peek());
-			stack.pop();
-			stack.show();
-		}
+		Queue<Integer> queue = new Queue<Integer>();
+		queue.enqueue(56);
+		queue.enqueue(70);
+		queue.enqueue(30);
+		queue.show();
 	}
 }
